60bb3812e62cbac5a39eed2567644a342c0c44b6
Tobias Schmoker <schmoker@novu.ch>
2016-04-27 12:34:47 +0200

    adds new service for receiving reduced extracts

--- src/app/components/oerebBern/oerebBern.service.js
+++ src/app/components/oerebBern/oerebBern.service.js
@@ -10,36 +10,12 @@ export class OerebBernService {
     }
 
     getExtractById(egrid) {
-        /*
-         https://example.com/oereb/extract/reduced/xml/CH887722167773
-         https://example.com/oereb/extract/reduced/xml/geometry/CH887722167773
-         https://example.com/oereb/extract/full/pdf/BE0200000332/100
-         */
 
-        // example xml: sandbox.novu.ch/oereb/DOC_362_0_995_CH.xml
+        let url = this.base + 'extract/reduced/json/' + egrid;
 
+        var promise = this.$http.get(url);
 
-        let exampleXMLUrl = 'http://sandbox.novu.ch/DOC_362_0_995_CH.xml';
-        this.$log.warn('getExtractById: ' + egrid);
-
-        this.$http.get(
-            exampleXMLUrl,
-            {
-                transformResponse: function (data) {
-                    // convert the data to JSON and provide
-                    // it to the success function below
-                    let x2js = new X2JS();
-                    let json = x2js.xml_str2json(data);
-                    return json;
-                }
-            }
-        ).success(function (data, status) {
-            // send the converted data back
-            // to the callback function
-            console.log(data);
-        });
-
-        return 'test';
+        return promise;
     }
 
 
