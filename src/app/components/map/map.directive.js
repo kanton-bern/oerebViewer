0b83bbb9b8c87a1836d2a134bf365c1d2ec7ddd9
sturm <bs@sturmundbraem.ch>
2016-05-02 08:25:20 +0200

    Removed subtitle from Header (as a result of uFive meeting, and in order to save some space in header)

--- src/app/components/map/map.directive.js
+++ src/app/components/map/map.directive.js
@@ -54,8 +54,8 @@ class MapController {
         });
 
 
-        var bottomSlider = $('.position-bottom');
-        var $themeTitle = $('.slide-title');
+        let bottomSlider = $('.position-bottom');
+        let $themeTitle = $('.slide-title');
         var vHeight = $(window).height() - 40;
         var $btnOpenTheme = $('#themeBottomToggler');
 
