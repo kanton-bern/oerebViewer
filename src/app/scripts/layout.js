f7c4dc93e4e53cdca69e649db2c209fdd20a1624
Tobias Schmoker <schmoker@novu.ch>
2016-05-10 19:08:43 +0200

    updates to new api changes (pierre honsbergers mail 18:40 - 10.5.16)

--- src/app/scripts/layout.js
+++ src/app/scripts/layout.js
@@ -15,4 +15,4 @@ $(window).on('load resize', function() {
         // Create breakpoint body.class
         // https://github.com/zurb/foundation/issues/5139
         console.log(Foundation.MediaQuery.current);
-})
+});
