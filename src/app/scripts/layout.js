358c71a8c728526b8f21f54a9f890a138de47950
sturm <bs@sturmundbraem.ch>
2016-05-10 10:32:06 +0200

    Introduction of content pages in main.html (as sections)

--- src/app/scripts/layout.js
+++ src/app/scripts/layout.js
@@ -1,5 +1,18 @@
 $(function() {
     console.log('loaded');
-
-
 });
+
+// Menubutton in header will change to cross [x] if menu is open.
+$contentWrapper = $('.content-wrapper');
+$panel = $('#menuLeftSlider');
+if ($panel.is(':visible')) {
+  // Foundation.Motion.animateOut($panel, 'slide-out-left');
+  console.log("Clicked");
+}
+
+
+$(window).on('load resize', function() {
+        // Create breakpoint body.class
+        // https://github.com/zurb/foundation/issues/5139
+        console.log(Foundation.MediaQuery.current);
+})
