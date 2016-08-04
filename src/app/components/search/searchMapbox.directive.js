8999c0db458ea8e93ec393ff8f2862f51c19e484
Tobias Schmoker <schmoker@novu.ch>
2016-08-04 10:32:26 +0200

    bugfix for address search

--- src/app/components/search/searchMapbox.directive.js
+++ src/app/components/search/searchMapbox.directive.js
@@ -24,23 +24,33 @@ class SearchMapboxController {
 
         // Initialize the suggestion engine with mapbox
         var placesSource = new Bloodhound({
-            limit: 30,
-            datumTokenizer: function(datum) {
-                return Bloodhound.tokenizers.whitespace(datum.name);
-            },
+            limit: 3,
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
             queryTokenizer: Bloodhound.tokenizers.whitespace,
             remote: {
                 url: "https://api.mapbox.com/geocoding/v5/mapbox.places/%QUERY.json?country=ch&access_token=pk.eyJ1Ijoibm92dSIsImEiOiJjaXJlb2Y4ZjUwMDBoaWdtMzU1azRoeTl6In0._jcWOL6ukPitI5BVcwOqeQ",
                 wildcard: "%QUERY",
                 // rateLimitWait: 1000,
                 filter: function(response) {
-                    return $.map(response.features, function(city) {
+                    console.log(response.features);
+
+                    var mapped = $.map(response.features, function(city) {
+                        // console.log(city);
+
                         return {
+                            id:  city.id,
                             name: city.place_name,
-                            longitude: city.geometry.coordinates[0],
-                            latitude: city.geometry.coordinates[1]
-                        }
+                            /*longitude: city.geometry.coordinates[0],
+                            latitude: city.geometry.coordinates[1]*/
+                            longitude: city.center[0],
+                            latitude: city.center[1],
+                            type: city.id.substr(0, city.id.indexOf('.'))
+                        };
                     });
+
+                    console.log(mapped);
+
+                    return mapped;
                 }
             }
         });
@@ -59,12 +69,13 @@ class SearchMapboxController {
             return self.search;
         }, function (value) {
             if (self.search !== null && typeof self.search === 'object') {
-
                 // center result
                 var coordinates = Coordinates.set('search', Coordinates.System[4326], [self.search.longitude, self.search.latitude]);
 
                 self.Map.setPosition(coordinates);
-                self.Map.click(coordinates);
+
+                if (self.search.type == "address")
+                    self.Map.click(coordinates);
             }
         });
 
