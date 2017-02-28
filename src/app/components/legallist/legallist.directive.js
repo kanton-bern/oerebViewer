14ba8ad0af5ccd51b3ab872a5e8a64ae3b0b8d07
Tobias Schmoker <schmoker@novu.ch>
2017-02-28 14:33:06 +0100

    filter legal documents on LegalListDirective level

--- src/app/components/legallist/legallist.directive.js
+++ src/app/components/legallist/legallist.directive.js
@@ -19,9 +19,109 @@ export function LegalListDirective() {
 }
 
 class LegalListController {
-    constructor() {
+    constructor($scope, Extracts) {
         'ngInject';
 
+        var self = this;
+        this.$scope = $scope;
         this.hasElement = false;
+        this.textList = [];
+
+
+
+        // fallback update
+        Extracts.registerCurrentObserverCallback(function() {
+            console.debug('manuel update')
+            console.debug(self.documents);
+        });
+
+
+        self.newDocumentsList = this.removeDuplicates(self.documents);
+    }
+
+
+    removeDuplicates(documents) {
+        var self = this;
+
+        var newDocumentsList = [];
+
+        documents.forEach(function (v) {
+
+            v.LegalProvisions.forEach(function (parentDocument) {
+
+                if (angular.isDefined(parentDocument.Reference)) {
+                    parentDocument.Reference.forEach(function (document) {
+                        if (document['_xsi:type'] != self.xsitype)
+                            return;
+
+                        if (self.isInTextList(document))
+                            return;
+
+                        self.fillTextList(document);
+
+                        newDocumentsList.push(document);
+
+                    });
+                }
+                // do the same for parent objects
+
+                // skip documents of other types
+                if (parentDocument['_xsi:type'] != self.xsitype)
+                    return;
+
+                if (self.isInTextList(parentDocument))
+                    return;
+
+                self.fillTextList(parentDocument);
+
+                newDocumentsList.push(parentDocument);
+            });
+        });
+
+        return newDocumentsList;
+    }
+
+    fillTextList(document) {
+        // add to textesList, if there is content
+        if (document.Title.LocalisedText.Text.length != 0) {
+            this.textList.push(document.Title.LocalisedText.Text);
+        }
+
+        if (document.OfficialTitle.LocalisedText.Text.length != 0) {
+            this.textList.push(document.OfficialTitle.LocalisedText.Text);
+        }
+
+        if (document.TextAtWeb.LocalisedText.Text.length != 0) {
+            this.textList.push(document.TextAtWeb.LocalisedText.Text);
+        }
+
+    }
+
+    isInTextList(document) {
+        var result = false;
+
+        if (document.Title.LocalisedText.Text.length != 0) {
+            result = this.isStringInTextList(document.Title.LocalisedText.Text);
+
+            if (result)
+                return result;
+        }
+
+        if (document.OfficialTitle.LocalisedText.Text.length != 0) {
+            result = this.isStringInTextList(document.OfficialTitle.LocalisedText.Text);
+
+            if (result)
+                return result;
+        }
+
+        if (document.TextAtWeb.LocalisedText.Text.length != 0) {
+            result = this.isStringInTextList(document.TextAtWeb.LocalisedText.Text);
+        }
+
+        return result;
+    }
+
+    isStringInTextList(string) {
+        return (this.textList.indexOf(string) > -1);
     }
 }
