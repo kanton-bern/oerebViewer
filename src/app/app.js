f749a28387e66b30cf7ebcbfa7264aa970971bc9
Tobias Schmoker <schmoker@novu.ch>
2019-06-05 13:32:09 +0200

    remove x2js

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,6 @@
 require('imports-loader?this=>window,define=>false,exports=>false!typeahead.js/dist/bloodhound');
 require('imports-loader?this=>window,define=>false,exports=>false!typeahead.js/dist/typeahead.jquery');
 require('imports-loader?this=>window,define=>false,exports=>false!./scripts/layout');
-require('imports-loader?this=>window,define=>false,exports=>false!x2js/xml2json');
 
 // load styling
 require('./index.scss');
