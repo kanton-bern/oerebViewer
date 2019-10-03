9b8d4a465bce99489c816dce6b6670168a4da1eb
Tobias Schmoker <schmoker@novu.ch>
2019-10-03 18:15:21 +0200

    corrections for missing legends and legal provisions

--- src/app/components/utilities/legals.filter.js
+++ src/app/components/utilities/legals.filter.js
@@ -1,9 +1,15 @@
-export function LegalsFilter () {
+export function LegalsFilter() {
     'ngInject';
 
     return function (values, xsitype) {
         let documents = values.flatMap((value) => {
-            return value.LegalProvisions;
+            let collection = value.LegalProvisions;
+            value.LegalProvisions.forEach(legalProvision => {
+                if (legalProvision.Reference) {
+                    collection = collection.concat(legalProvision.Reference)
+                }
+            });
+            return collection
         }).filter(document => {
             return document.DocumentType === xsitype;
         });
