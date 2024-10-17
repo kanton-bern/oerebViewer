3f5261db2a1f0779514ed2e305cc19ea0b5ff94e
Bernhard Sturm <bs@sturmundbraem.ch>
2024-10-17 16:43:15 +0200

    [ADD] toast notification styles

---
+++ .output/server/chunks/build/client.manifest.mjs
@@ -0,0 +1,309 @@
+const client_manifest = {
+  "_BY76vVlO.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BY76vVlO.js",
+    "name": "app",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "dynamicImports": [
+      "config/bern/setup.js",
+      "config/graubuenden/setup.js"
+    ]
+  },
+  "_CainRnEa.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CainRnEa.js",
+    "name": "vue.8fc199ce",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_D1jM8M2-.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D1jM8M2-.js",
+    "name": "nuxt-link",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "config/bern/locales/de.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C-w8tqag.js",
+    "name": "de",
+    "src": "config/bern/locales/de.json",
+    "isDynamicEntry": true
+  },
+  "config/bern/locales/fr.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BAeuqBCV.js",
+    "name": "fr",
+    "src": "config/bern/locales/fr.json",
+    "isDynamicEntry": true
+  },
+  "config/bern/setup.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "gLoDn2Cr.js",
+    "name": "setup",
+    "src": "config/bern/setup.js",
+    "isDynamicEntry": true
+  },
+  "config/defaults/locales/de.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Cp-Thvn1.js",
+    "name": "de",
+    "src": "config/defaults/locales/de.json",
+    "isDynamicEntry": true
+  },
+  "config/defaults/locales/en.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BIHI7g3E.js",
+    "name": "en",
+    "src": "config/defaults/locales/en.json",
+    "isDynamicEntry": true
+  },
+  "config/defaults/locales/fr.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CuORBMMl.js",
+    "name": "fr",
+    "src": "config/defaults/locales/fr.json",
+    "isDynamicEntry": true
+  },
+  "config/defaults/locales/rm.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DUux0Bfc.js",
+    "name": "rm",
+    "src": "config/defaults/locales/rm.json",
+    "isDynamicEntry": true
+  },
+  "config/graubuenden/locales/de.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Df6KNeSA.js",
+    "name": "de",
+    "src": "config/graubuenden/locales/de.json",
+    "isDynamicEntry": true
+  },
+  "config/graubuenden/locales/fr.json": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "FmFgRqLi.js",
+    "name": "fr",
+    "src": "config/graubuenden/locales/fr.json",
+    "isDynamicEntry": true
+  },
+  "config/graubuenden/setup.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "B59iZ5kO.js",
+    "name": "setup",
+    "src": "config/graubuenden/setup.js",
+    "isDynamicEntry": true
+  },
+  "i18n.config.ts?hash=bffaebcb&config=1": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DhjAV9ci.js",
+    "name": "i18n.config",
+    "src": "i18n.config.ts?hash=bffaebcb&config=1",
+    "isDynamicEntry": true
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "lkNUGiWC.js",
+    "name": "default",
+    "src": "layouts/default.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BY76vVlO.js",
+      "_D1jM8M2-.js"
+    ],
+    "css": [
+      "default.CaxWJ1nk.css"
+    ]
+  },
+  "default.CaxWJ1nk.css": {
+    "file": "default.CaxWJ1nk.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "locales/de.js?hash=826d5bdf&locale=de": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": false,
+    "preload": false,
+    "file": "CZggmg2r.js",
+    "name": "de",
+    "src": "locales/de.js?hash=826d5bdf&locale=de",
+    "isDynamicEntry": true
+  },
+  "locales/fr.js?hash=c04a0741&locale=fr": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": false,
+    "preload": false,
+    "file": "MQWeZZ0a.js",
+    "name": "fr",
+    "src": "locales/fr.js?hash=c04a0741&locale=fr",
+    "isDynamicEntry": true
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Bl1ibIUz.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_D1jM8M2-.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_CainRnEa.js"
+    ],
+    "css": [
+      "error-404.ygbHJO5Q.css"
+    ]
+  },
+  "error-404.ygbHJO5Q.css": {
+    "file": "error-404.ygbHJO5Q.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D4ndaafE.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_CainRnEa.js"
+    ],
+    "css": [
+      "error-500.B11Ibp8J.css"
+    ]
+  },
+  "error-500.B11Ibp8J.css": {
+    "file": "error-500.B11Ibp8J.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Culu95Kp.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "layouts/default.vue",
+      "locales/de.js?hash=826d5bdf&locale=de",
+      "locales/fr.js?hash=c04a0741&locale=fr",
+      "i18n.config.ts?hash=bffaebcb&config=1",
+      "config/defaults/locales/de.json",
+      "config/defaults/locales/en.json",
+      "config/defaults/locales/fr.json",
+      "config/defaults/locales/rm.json",
+      "config/bern/locales/de.json",
+      "config/bern/locales/fr.json",
+      "config/defaults/locales/de.json",
+      "config/defaults/locales/en.json",
+      "config/defaults/locales/fr.json",
+      "config/defaults/locales/rm.json",
+      "config/graubuenden/locales/de.json",
+      "config/graubuenden/locales/fr.json",
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.DM0zDlXJ.css"
+    ]
+  },
+  "entry.DM0zDlXJ.css": {
+    "file": "entry.DM0zDlXJ.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/[...slug].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DSoXBAj_.js",
+    "name": "_...slug_",
+    "src": "pages/[...slug].vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_BY76vVlO.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/colors.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D7Dx5RJN.js",
+    "name": "colors",
+    "src": "pages/colors.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
